a67804b4d903d0b595196d6e72b2849363c11ec8
Solano <solanoepalacio@gmail.com>
2020-02-13 01:04:32 -0300

    fix security groups and add dns

--- stacks/stateless-web-app/deploy.sh
+++ stacks/stateless-web-app/deploy.sh
@@ -13,6 +13,12 @@ aws cloudformation create-stack --stack-name $STACK_NAME \
     --template-body file://aws-infrastructure.yml \
     --parameters \
         ParameterKey=EC2KeyName,ParameterValue=$EC2_KEY_NAME \
-        ParameterKey=NodeServerUserData,ParameterValue=$(base64 -w0 $NODE_SERVER_USER_DATA_SCRIPT) \
+        ParameterKey=NodeServerUserData,ParameterValue=$(base64 -w0 $NODE_SERVER_USER_DATA_SCRIPT) 
 
 
+# aws cloudformation create-stack \
+#     --stack-name $STACK_NAME \
+#     --template-body file://aws-infrastructure.yml \
+    # --parameters \
+        # ParameterKey=EC2KeyName,ParameterValue=$EC2_KEY_NAME \
+        # ParameterKey=NodeServerUserData,ParameterValue=$(base64 -w0 $NODE_SERVER_USER_DATA_SCRIPT) \
