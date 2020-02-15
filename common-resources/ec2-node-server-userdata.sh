e78c0850590b2d330dfc6cf941448538b1433788
Solano <solanoepalacio@gmail.com>
2020-02-14 22:58:52 -0300

    update user-data script

--- common-resources/ec2-node-server-userdata.sh
+++ common-resources/ec2-node-server-userdata.sh
@@ -7,7 +7,7 @@ sudo yum install -y nodejs
 
 mkdir code && cd code;
 
-curl -H "Cache-Control: no-cache" https://gist.githubusercontent.com/solanoepalacio/61409de5f74c88c754ca1f1d044e81fa/raw/c25e4abbe9c3a79f682d24b75f1422558785da9f/server.js > server.js
+curl -H "Cache-Control: no-cache" https://gist.githubusercontent.com/solanoepalacio/61409de5f74c88c754ca1f1d044e81fa/raw/0ccc8285e18c428161a1f021881d2ddcf2457473/server.js > server.js
 
 echo "Server start command ran at: $(date)" >> logs
 
